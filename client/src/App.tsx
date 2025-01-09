95cc18aa21c272a1e01a6df7c2c9e29a19be3b7e
André Ferreira <hello@andreferreira.me>
2025-01-09 01:56:10 +0000

    Add React Router and start sections

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <button className="btn btn-primary">Button</button>
+      {/* Navbar */}
+      <div className="navbar bg-base-100">
+        <a className="btn btn-ghost text-xl">Hoag!</a>
+      </div>
+
+      {/* Main */}
+      <div className="main">
+        <h2 className="text-xl font-bold text-left">Blocks</h2>
+        <h2 className="text-xl font-bold text-left">Transactions</h2>
+        <h2 className="text-xl font-bold text-left">Wallet</h2>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
